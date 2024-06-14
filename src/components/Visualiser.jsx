44eb1cd90680f0292a7ae0878d91bc4b0e65c95f
James Gore <83005220+thejamesgore@users.noreply.github.com>
2024-06-14 03:51:04 +0100

    add css comparing bars during sort

--- src/components/Visualiser.jsx
+++ src/components/Visualiser.jsx
@@ -4,16 +4,18 @@ export default function Visualizer({ array, animations }) {
   const [localArray, setLocalArray] = useState(array)
   const [localAnimations, setLocalAnimations] = useState([])
   const [comparingIndices, setComparingIndices] = useState([-1, -1])
+  const [sortedIndices, setSortedIndices] = useState([])
 
   useEffect(() => {
     setLocalArray(array)
     setLocalAnimations(animations)
+    setSortedIndices([]) // Reset sorted indices when a new array is set
   }, [array, animations])
 
   useEffect(() => {
     if (localAnimations.length === 0) return
 
-    const [i, j] = localAnimations[0]
+    const [i, j, isFinalPosition] = localAnimations[0]
     setComparingIndices([i, j])
 
     const timer = setTimeout(() => {
@@ -22,6 +24,10 @@ export default function Visualizer({ array, animations }) {
       setLocalArray(newArray)
       setLocalAnimations(localAnimations.slice(1))
       setComparingIndices([-1, -1])
+
+      if (isFinalPosition) {
+        setSortedIndices((prev) => [...prev, j])
+      }
     }, 10)
 
     return () => clearTimeout(timer)
@@ -32,9 +38,9 @@ export default function Visualizer({ array, animations }) {
       {localArray.map((value, index) => (
         <div
           key={index}
-          className={`bar ${
-            comparingIndices.includes(index) ? 'bar-being-compared' : ''
-          }`}
+          className={`bar 
+            ${comparingIndices.includes(index) ? 'bar-being-compared' : ''} 
+            ${sortedIndices.includes(index) ? 'bar-sorted' : ''}`}
           style={{ height: `${value}px` }}
         ></div>
       ))}
