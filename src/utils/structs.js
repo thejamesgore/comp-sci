a8ebc1970cea7ce4f8f77df17cd5c4395fe486c2
James Gore <83005220+thejamesgore@users.noreply.github.com>
2024-06-23 23:13:49 +0100

    add: new linked list with methods

--- src/utils/structs.js
+++ src/utils/structs.js
@@ -4,3 +4,44 @@ class Node {
     this.value = value
   }
 }
+
+class LinkedList {
+  constructor() {
+    this.head = null
+  }
+
+  append(value) {
+    // We create a new node that will be appeneded to the linked list
+    const newNode = new Node(value)
+    // If there's no linked list yet there will be no head, so we create a new node and point the head towards it. Then we will have one node.
+    if (!this.head) {
+      this.head = newNode
+      return newNode
+    }
+
+    // If we already have at least one or more nodes... we create a pointer that will travel along the linked list.
+    // It will point towards the top of the Linked list at the head as we can only guarentee this nodes position.
+    let current = this.head
+
+    // We iterate from node to node until we reach one which points to null. This will be the last node
+    while (current.next) {
+      current = current.next
+    }
+    // then we add the new node to the linked list by pointing the last node of the list to the new node.
+    current.next = newNode
+
+    // we then return the new node
+    return newNode
+  }
+
+  //   Helper function to convert LL into array for react.
+  toArray() {
+    const elements = []
+    let current = this.head
+    while (current) {
+      elements.push(current.value)
+      current = current.next
+    }
+    return elements
+  }
+}
